4d7737f9ece2b0fb23c9fa54d7d9721aacbf2774
Behnam Yousefi <46751569+behnam-yousefi@users.noreply.github.com>
2022-12-08 21:01:15 +0100

    Document update

--- usage_examples/ISN_ex.R
+++ usage_examples/ISN_ex.R
@@ -10,5 +10,8 @@ embeddingSpaceList = mnda_embedding(graph_data, train.rep=50, walk.rep=10,
                                            epochs = 20, batch.size = 10,
                                            random.walk=FALSE)
 
-Results = mnda_node_detection_isn(embeddingSpaceList, stat.test = "t.test", 
+Results = mnda_node_detection_isn(embeddingSpaceList, stat.test = "t.test",
                                   p.adjust.method = "BH")
+
+embeddingSpaceList = mnda_embedding(graph.data=graph_data, outcome=c(1,2), indv.index = c(1,1), train.rep=5, walk.rep=5)
+Dist = mnda_node_distance(embeddingSpaceList)
