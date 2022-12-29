7bd30e08a3aba17e7f45f9b8eacab61f32804e16
Behnam Yousefi <46751569+behnam-yousefi@users.noreply.github.com>
2022-12-29 16:03:19 +0100

    mnda v1.0.0 ISN_ex.R file updated

--- usage_examples/ISN_ex.R
+++ usage_examples/ISN_ex.R
@@ -3,19 +3,21 @@ rm(list=ls())
 library(mnda)
 setwd("~/Desktop/R_Root/MNDA/usage_examples/")
 
-data = data.frame(readRDS("Data/ISN_net.rds"))
+data = readRDS("Data/ISN_BCG.rds")
 nodeList = t(sapply(rownames(data), function(x) strsplit(x,"_")[[1]]))
 
 y = colnames(data)
 y = data.frame(t(data.frame(strsplit(y, "_"))))
+colnames(y) = c("ID", "Stim", "Sex")
+sex = y[duplicated(y$ID), "Sex"]
 
 graph_data = cbind(nodeList, data)
-embeddingSpaceList = mnda_embedding(graph_data, outcome = y[,2], indv.index = y[,1],
+embeddingSpaceList = mnda_embedding(graph_data, outcome = y$Stim, indv.index = y$ID,
                                     train.rep=2, walk.rep=10, epochs=5, batch.size=100,
                                     random.walk=FALSE)
 
 Dist = mnda_node_distance(embeddingSpaceList)
-Pval = mnda_distance_test_isn(Dist, rep(c(1,2),25))
+Pval = mnda_distance_test_isn(Dist, sex)
 
 
 
